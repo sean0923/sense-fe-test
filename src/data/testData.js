b711cce4be5de53dbe3d562291220c3dd538b600
Sean Hong <iamseanhong@gmail.com>
2019-02-21 17:43:27 -0800

    Add test data

---
+++ src/data/testData.js
@@ -0,0 +1,14 @@
+export default {
+  id_1: {
+    id: 'id_1',
+    name: 'sean',
+    email: 'sean@gmail.com',
+    tel: '123-123-1233',
+  },
+  id_2: {
+    id: 'id_2',
+    name: 'sean',
+    email: 'sean@gmail.com',
+    tel: '123-123-1233',
+  },
+};
